fc2b1668b2b73a8a307ee01136834196df5a3bfa
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-05 22:32:52 +0300

    javascript

--- accordion/script.js
+++ accordion/script.js
@@ -1,1 +1,49 @@
-console.log("accordion");
+class Accordion {
+  constructor(domNode) {
+    this.rootEl = domNode;
+    this.buttonEl = this.rootEl.querySelector("button[aria-expanded]");
+
+    const controlsId = this.buttonEl.getAttribute("aria-controls");
+    this.contentEl = document.getElementById(controlsId);
+
+    this.open = this.buttonEl.getAttribute("aria-expanded") === true;
+
+    // Event listeners
+    this.buttonEl.addEventListener("click", this.onButtonClick.bind(this));
+  }
+
+  toggle(open) {
+    if (open === this.open) {
+      return;
+    }
+
+    // update the internal state
+    this.open = open;
+
+    // handle DOM updates
+    this.buttonEl.setAttribute("aria-expanded", `${open}`);
+    if (open) {
+      this.contentEl.removeAttribute("hidden");
+    } else {
+      this.contentEl.setAttribute("hidden", "");
+    }
+  }
+
+  onButtonClick() {
+    this.toggle(!this.open);
+  }
+
+  open() {
+    this.toggle(true);
+  }
+
+  close() {
+    this.toggle(false);
+  }
+}
+
+// init accordions
+// const accordions = document.querySelectorAll(".accordion h3");
+// accordions.forEach((accordionEl) => {
+//   new Accordion(accordionEl);
+// });
