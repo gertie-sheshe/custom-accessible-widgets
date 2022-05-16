43aead0e420ea4d04f7ca104af9e41bbcc0de5f1
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-05-16 14:34:26 +0300

    hover

--- carousel/main.js
+++ carousel/main.js
@@ -98,4 +98,37 @@ const CarouselPreviousNext = function (node, options) {
   }
 
   // Carousel Item Events
+  for (let i = 0; i < this.carouselItemNodes.length; i++) {
+    let carouselItemNode = this.carouselItemNodes[i];
+
+    // support stopping rotation when any element receives focus in the tabpanel
+    carouselItemNode.addEventListener("focusin", this.handleFocusIn.bind(this));
+    carouselItemNode.addEventListener(
+      "focusout",
+      this.handleFocusOut.bind(this)
+    );
+
+    let imageLinkNode = carouselItemNode.querySelector(".carousel-image a");
+
+    if (imageLinkNode) {
+      imageLinkNode.addEventListener(
+        "focus",
+        this.handleImageLinkFocus.bind(this)
+      ); // NOTE: Convert these to rrow functions Gertie!
+      imageLinkNode.addEventListener(
+        "blur",
+        this.handleImageLinkBlur.bind(this)
+      );
+    }
+  }
+
+  // Handle hover events
+  this.domNode.addEventListener("mouseover", this.handleMouseOver.bind(this));
+  this.domNode.addEventListener("mouseout", this.handleMouseOut.bind(this));
+
+  // Initialize behaviour based on options
+  this.enableOrDisableAutoRotation(options.norotate);
+  this.updatePlaying(!options.paused && !options.norotate);
+  this.setAccessibleStyling(options.moreaccessible);
+  this.rotateSlides();
 };
